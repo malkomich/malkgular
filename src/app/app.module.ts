7d644928e0a3045e1fe635e79fdaa5cb5cbd570c
Juan Carlos González <jcgonzalez@undanet.com>
2017-03-16 18:04:20 +0100

    Add more polymer dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
@@ -19,6 +19,7 @@ import { AppComponent } from './app.component';
   ],
   providers: [],
   entryComponents: [ AppComponent ],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
